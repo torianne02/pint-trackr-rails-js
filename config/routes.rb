118b8a4f4e938d91f8055b6c4cc19ecc929cdc5c
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-07 15:29:03 -0700

    creates routes within config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  get '/auth/facebook/callback' => 'sessions#create'
+  root 'static_pages#home'
+  resources :users do
+    resources :beers
+  end
+  resources :breweries
+
+  get '/signin', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  post '/users/new', to: 'users#create'
+  get '/logout', to: 'sessions#destroy'
+  get '/auth/facebook/callback' => 'sessions#create_from_oath'
 end
