355a94b627699230154c01a2254ce8cc19beeaad
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-07 12:44:49 -0700

    creates user model adds validations to user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :beers
+  has_many :breweries, through: :beers
+
+  validates :username, :email, :password, :presence => true
+end
