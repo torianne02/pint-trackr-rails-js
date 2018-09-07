525cdbabea9f267ad4ea74d4c4cf08d9a59bd4d6
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-07 13:14:09 -0700

    adds user create method to controller adds before_action methods to controller ;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+  before_action :current_user, except: %i[new create]
+
   def new
     @user = User.new
   end
 
   def create
-  end 
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "Welcome to PintTrackr, #{@user.username}!"
+    else
+      redirect_to root_path
+    end
+  end
 
 
   private
