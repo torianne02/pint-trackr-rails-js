fe9d56ede1f61e0df98fc6c2ef7fc90780d02717
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-07 13:34:31 -0700

    creates sessions controller creates all methods within controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new, notice: 'Username and/or password are incorrect.'
+    end
+  end
+
+  def create_from_oath
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.username = auth['info']['name']
+      u.email = auth['info']['email']
+    end
+
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_url
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+end
