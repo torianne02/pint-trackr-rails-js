c6227059cc4b9cb73b2ef0d1d3d71787d46301a9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-10 14:25:41 -0700

    fixes bugs adds delete option for beers adds nav bar to layout

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,8 +1,4 @@
 class BreweriesController < ApplicationController
-  def index
-    @breweries = Brewery.all
-  end
-
   def show
     @brewery = Brewery.find_by(id: params[:id])
     @beers = @brewery.beers
