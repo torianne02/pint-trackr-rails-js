fc7828867ff70e1db1e9481467526ced53cd2687
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-11 11:35:39 -0700

    removes use of breweries/show page comments out breweries/show.html.erb for future use adds comment about changing beer model structure for future use eliminates link_to brewery/show page in all files adds edit beer link to beer/show page

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,6 +2,7 @@ class Beer < ActiveRecord::Base
   belongs_to :user
   belongs_to :brewery
   accepts_nested_attributes_for :brewery
+  # has_many :users -- changes for future versions
 
   validates :name, :beer_type, :ibu, :abv, :presence => true
 end
