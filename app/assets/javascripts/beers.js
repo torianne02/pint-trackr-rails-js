b2677d3d2a835a44d1463dbc858ef16de7862ac8
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-29 10:50:28 -0700

    adds JS beer object

---
+++ app/assets/javascripts/beers.js
@@ -0,0 +1,9 @@
+// JSON Beer Constructor
+function Beer(attributes) {
+  this.name = attributes.name;
+  this.beer_type = attributes.beer_type;
+  this.ibu = attributes.ibu;
+  this.abv = attributes.abv;
+  this.brewery_id = attributes.brewery_id;
+  this.user_id = attributes.user_id;
+}
