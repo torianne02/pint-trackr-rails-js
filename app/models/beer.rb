8afc004dd5236ebf9f4903ce0c95da1b169b2e32
torianne02 <victoria.fluharty@gmail.com>
2018-11-13 08:46:05 -0800

    cleans up code in beer model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,7 +2,6 @@ class Beer < ActiveRecord::Base
   belongs_to :user
   belongs_to :brewery
   accepts_nested_attributes_for :brewery
-  # has_many :users -- changes for future versions
 
   validates :name, :beer_type, :ibu, :abv, :presence => true
   scope :highest_ibu, -> {order(ibu: :desc)}
