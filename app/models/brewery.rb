aa9d84f29cbb28e1d1a178b3529e41da461f9976
torianne02 <victoria.fluharty@gmail.com>
2018-12-06 16:21:10 -0800

    updates validations to remove city and state for brewery

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,5 +2,5 @@ class Brewery < ActiveRecord::Base
   has_many :beers
   has_many :users, through: :beers
 
-  validates :name, :city, :state, :presence => true
+  validates :name, :presence => true
 end
