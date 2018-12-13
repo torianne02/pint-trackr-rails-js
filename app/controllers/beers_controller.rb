215390be215e3abb89db8e2fe6d72609b3caee9d
torianne02 <victoria.fluharty@gmail.com>
2018-12-13 15:38:20 -0800

    removes use of scope method in beers#index

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -3,7 +3,7 @@ class BeersController < ApplicationController
   before_action :set_beer, only: %i[show edit update destroy]
 
   def index
-    @beers = @user.beers.highest_ibu
+    @beers = @user.beers
     respond_to do |format|
       format.html
       format.json { render json: @beers }
