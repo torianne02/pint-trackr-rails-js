70a54818571bd78a9776db40d0f8dfbf6fd9873d
torianne02 <victoria.fluharty@gmail.com>
2018-12-17 13:08:40 -0800

    adds logic for next and previous buttons in beer.js

--- app/assets/javascripts/beers.js
+++ app/assets/javascripts/beers.js
@@ -6,6 +6,8 @@ function Beer(name, beer_type, ibu, abv, brewery_id, user_id) {
   this.abv = abv
   this.brewery_id = brewery_id
   this.user_id = user_id
+  this.idOfNext = 0
+  this.idOfLast = 0
 }
 
 // clear form
@@ -24,7 +26,23 @@ Beer.prototype.successMessage = function() {
 
 // next beer function
 function getBeer(data) {
-  var beer = data
+  const beer = data
+  const userBeers = beer.user.beers
+
+  // beer checks
+  const lastBeerCheck = () => userBeers[userBeers.indexOf(findBeer[0]) - 1]
+  const nextBeerCheck = () => userBeers[userBeers.indexOf(findBeer[0]) + 1]
+
+  // set id functions
+  const setIdOfNext = () => userBeers[userBeers.indexOf(findBeer[0]) + 1].id
+  const setIdOfLast = () => userBeers[userBeers.indexOf(findBeer[0]) - 1].id
+
+  // find beer
+  const findBeer = userBeers.filter(userBeer => userBeer.id === beer.id)
+
+  // set id's
+  beer.idOfNext = nextBeerCheck() ? setIdOfNext() : 0
+  beer.idOfLast = lastBeerCheck() ? setIdOfLast() : 0
 
   $('div#show_beer').html("")
   $('div#show_beer').html(`<h3>${beer.name}</h3>
@@ -32,8 +50,9 @@ function getBeer(data) {
     <p>Beer Type: ${beer.beer_type}</p>
     <p>IBU: ${beer.ibu}</p>
     <p>ABV: ${beer.abv}</p>
+    <a href="/beers/${beer.idOfLast}" class="prev_beer">Previous Beer</a>
     <a href="/beers/${beer.id}/edit">Edit</a>
-    <a href="/beers/${beer.id + 1}" class="next_beer">Next Beer</a>`)
+    <a href="/beers/${beer.idOfNext}" class="next_beer">Next Beer</a>`)
 }
 
 $(function() {
@@ -56,7 +75,24 @@ $(function() {
   })
 
   // show next beer request
-  $('.next_beer').on('click', function(e) {
+  $('#show_beer').on('click', 'a.next_beer', function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "GET",
+      url: this.href,
+      data: $(this).serialize(),
+      dataType: 'json',
+      success: function(response) {
+        getBeer(response)
+      },
+      error: function(response) {
+        alert("Oops! Something went wrong!")
+      }
+    })
+  })
+
+  // show previous beer request
+  $('#show_beer').on('click', 'a.prev_beer', function(e) {
     e.preventDefault();
     $.ajax({
       type: "GET",
