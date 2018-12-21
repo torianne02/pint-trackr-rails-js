b37c66a86f60276128d33399a21d051f43cb58da
torianne02 <victoria.fluharty@gmail.com>
2018-12-21 15:32:44 -0800

    adds brewery routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users
   resources :beers
+  resources :breweries
 
   get '/signin', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
